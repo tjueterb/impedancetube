91da4da58ffb7953b263e5140ea5550c4e4fc4ed
Tobias Jüterbock <t.jueterbock@campus.tu-berlin.de>
2022-09-05 13:21:57 +0200

    Rename TAP tubes in init file

--- impedancetube/__init__.py
+++ impedancetube/__init__.py
@@ -1,5 +1,5 @@
 from .measurement import Measurement, Measurement_E2611, MicSwitchCalib_E2611, Measurement_ISO10534
 from .tube import Tube, Tube_Impedance, Tube_Transmission, \
     Tube_Impedance_TAP_rect_25cm_narrow, Tube_Impedance_TAP_rect_25cm_wide, \
-    Tube_TAP_Impedance_rect_50cm_narrow, Tube_Impedance_TAP_rect_50cm_wide, \
+    Tube_Impedance_TAP_rect_50cm_narrow, Tube_Impedance_TAP_rect_50cm_wide, \
     Tube_Transmission_TAP_rect_narrow, Tube_Transmission_TAP_rect_wide
