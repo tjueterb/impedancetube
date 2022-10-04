d4ebd83752c05d160207e3e20a524da5950a2ecc
Tobias Jüterbock <t.jueterbock@campus.tu-berlin.de>
2022-10-04 17:04:27 +0200

    Synthesize example data with impulse and delays

--- examples/Synthesize ISO 10534 example data.py
+++ examples/Synthesize ISO 10534 example data.py
@@ -8,16 +8,17 @@ import h5py
 fs = 44100
 c = 20.047 * np.sqrt(273.15 + 20) # speed of sound at 20 °C
 
-#input noise
-noise = pf.signals.noise(n_samples=4*fs, spectrum='white',rms=0.1, sampling_rate=fs)
+#input signal
+# input = pf.signals.noise(n_samples=4*fs, spectrum='white',rms=0.1, sampling_rate=fs)
+input = pf.signals.impulse(n_samples=4*fs, sampling_rate=fs, amplitude=0.7, delay=50)
 
-# incident signal at position 1 (can be omitted)
+# incident signal at position 1 (time delay can be omitted)
 dirac = pf.signals.impulse(n_samples=1, sampling_rate=fs)
-pos1_incident = pf.dsp.convolve(signal1=noise, signal2=dirac)
+pos1_incident = pf.dsp.convolve(signal1=input, signal2=dirac)
 
 # delay input from position 1 to position 2
-s_narrow = 0.45
-dt = s_narrow / c
+s_12 = 0.45
+dt = s_12 / c
 dN = int(dt * fs)
 dirac_pos1_pos2 = pf.signals.impulse(n_samples=dN+1, delay=dN, sampling_rate=fs)
 
@@ -53,15 +54,17 @@ pos2_reflected = pf.dsp.convolve(signal1=pos3_reflected, signal2=dirac_pos2_pos3
 pos1_reflected = pf.dsp.convolve(signal1=pos2_reflected, signal2=dirac_pos1_pos2)
 
 # crop signals and add incident and reflected signal
-pos1_incident  = pf.dsp.time_window(signal=pos1_incident,  window='boxcar', interval=[fs,3*fs-1], crop='window')
-pos1_reflected = pf.dsp.time_window(signal=pos1_reflected, window='boxcar', interval=[fs,3*fs-1], crop='window')
-pos2_incident  = pf.dsp.time_window(signal=pos2_incident,  window='boxcar', interval=[fs,3*fs-1], crop='window')
-pos2_reflected = pf.dsp.time_window(signal=pos2_reflected, window='boxcar', interval=[fs,3*fs-1], crop='window')
-pos3_incident  = pf.dsp.time_window(signal=pos3_incident,  window='boxcar', interval=[fs,3*fs-1], crop='window')
-pos3_reflected = pf.dsp.time_window(signal=pos3_reflected, window='boxcar', interval=[fs,3*fs-1], crop='window')
+start = 0
+stop = fs-1
+pos1_incident  = pf.dsp.time_window(signal=pos1_incident,  window='boxcar', interval=[start,stop], crop='window')
+pos1_reflected = pf.dsp.time_window(signal=pos1_reflected, window='boxcar', interval=[start,stop], crop='window')
+pos2_incident  = pf.dsp.time_window(signal=pos2_incident,  window='boxcar', interval=[start,stop], crop='window')
+pos2_reflected = pf.dsp.time_window(signal=pos2_reflected, window='boxcar', interval=[start,stop], crop='window')
+pos3_incident  = pf.dsp.time_window(signal=pos3_incident,  window='boxcar', interval=[start,stop], crop='window')
+pos3_reflected = pf.dsp.time_window(signal=pos3_reflected, window='boxcar', interval=[start,stop], crop='window')
 
-signal_pos1 = pos1_incident + pos1_reflected
-signal_pos2 = pos2_incident + pos2_reflected
+signal_pos1 = pos1_incident + pos1_reflected 
+signal_pos2 = pos2_incident + pos2_reflected 
 signal_pos3 = pos3_incident + pos3_reflected
 
 # TODO: mix signals to one object with 3 channels (acoular excpects array of shape [N_samples x N_channels])
